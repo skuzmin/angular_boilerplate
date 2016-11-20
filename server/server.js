55ed8b9461ccdf27de655c6628e430b40bf16ad9
skuzmin <TrueJarod@Sergiys-MacBook-Pro.local>
2016-11-21 00:57:21 +0200

    removed unused stuff

--- server/server.js
+++ server/server.js
@@ -1,11 +1,7 @@
 var express = require('express'),
 	app = express(),
-	path = require('path'),
-    http = require('http'),
-	httpServer = http.Server(app),
-	target = '/app',
 	port = 3001,
 	host = 'localhost';
 	
 app.listen(port, host);
-console.log('Server is runnig at ' + host + ':' + port + '\nTarget: ' + target);
+console.log('Server is runnig at ' + host + ':' + port);
